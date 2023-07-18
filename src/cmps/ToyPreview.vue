01cec0438cee871cb2da44387456321210304ece
LidorLevi123 <lidorlevi728@gmail.com>
2023-07-18 18:12:57 +0300

    Add Toy Preview

---
+++ src/cmps/ToyPreview.vue
@@ -0,0 +1,15 @@
+<template>
+    <article class="toy-preview">
+        <img :src="`https://robohash.org/ + ${ toy.name }`">
+        <span class="title">{{ toy.name }}</span>
+        <span class="price">${{ toy.price }}</span>
+    </article>
+</template>
+
+<script>
+export default {
+    props: {
+        toy: { type: Object, required: true },
+    }
+}
+</script>
