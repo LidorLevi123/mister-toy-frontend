182f6e8ba63bf579f8b7030cf03b42c993506f07
LidorLevi123 <lidorlevi728@gmail.com>
2023-07-18 18:12:45 +0300

    Add Toy List

---
+++ src/cmps/ToyList.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="toy-list">
+        <ul class="clean-list">
+            <li v-for="toy in toys" :key="toy._id">
+                <section class="actions">
+                    <button @click="removeToy(toy)">x</button>
+                </section>
+                <ToyPreview :toy="toy"/>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+import ToyPreview from '@/cmps/ToyPreview.vue'
+
+export default {
+    props: {
+        toys: { type: Array, required: true }
+    },
+
+    methods: {
+        removeToy(toy){
+            this.$emit('remove', toy)
+        }
+    },
+
+    components: {
+        ToyPreview,
+    }
+}
+</script>
